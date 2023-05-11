c200b5c2b3d687540a84df53d1fc58f13ff3150c
AkrmYoussef <dr.akrmyoussef410@gmail.com>
2023-05-11 20:13:07 +0300

    "commit"

---
+++ src/OperatingSystem.java
@@ -0,0 +1,25 @@
+public class OperatingSystem {
+    // print
+
+    // assign
+
+    // writeFile
+
+    // readFile
+
+    // printFromTo
+
+
+    // semWait
+
+
+
+    // semSignal
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
+
